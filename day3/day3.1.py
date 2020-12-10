57b35f56820e3309c46572c26550c2b64c523e3d
Fernandez, Jesus [Global Data & Content] <Jesus.Fernandez@solera.com>
2020-12-10 07:36:18 +0100

    Project restructured. Day 6.1 completed

--- day3/day3.1.py
+++
@@ -1,9 +0,0 @@
-
-def is_tree(line, position):
-    return (line[position % len(line)] == '#')
-
-with open("puzzle.txt", "r") as f:
-    forest = f.readlines()
-    forest.pop(0)
-    result = sum(1 for index, line in enumerate(forest, start=1) if is_tree(line.rstrip(), index * 3))
-    print(result)
